aa4cd8bdaf0a37511a129d7a7f7bdfe6fb323e56
Alex <141788592+Bladilob12@users.noreply.github.com>
2023-10-08 18:15:40 -0300

    commit

---
+++ src/main/java/FiltroPara.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import email.Buzon;
+import email.Contacto;
+import email.Correo;
+
+public class FiltroPara {
+    public void filtrarPara(Correo correo){
+
+        List<Buzon> listaMailsUsuario = new ArrayList<>();
+
+        for (Contacto destinatario : correo.para) {
+            Predicate<Buzon> buscarBuzon = b -> destinatario.getMail().equals(b.getMail());
+            listaMailsUsuario.addAll(listaMailsUsuario.stream().filter(buscarBuzon).collect(Collectors.toList()));
+        }
+        for (Buzon buzon : listaMailsUsuario) {
+            buzon.bandejaEntrada.add(correo);
+        }
+    }
+}
